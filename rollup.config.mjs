934b70f6257ee5f9fbc1d49dd2b58a99aa3b163a
mattcoleanderson <github@mattcoleanderson.com>
2023-10-27 02:24:34 -0500

    Add inline sourcemaps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
 
+// TODO: Create environment specific builds (dev, prod, etc.)
+// Dataview plugin has a good implementaiton of this
 export default {
 	input: 'src/main.ts',
 	output: {
-		file: 'main.js',
+		file: 'out/main.js',
 		format: 'es',
+		sourcemap: 'inline'
 	},
 	plugins: [typescript()],
 	external: ['obsidian', 'path']
