fe2818cea348f3e20165828bf18a92eb4fa1ff04
mattcoleanderson <github@mattcoleanderson.com>
2023-10-31 15:29:05 -0500

    Fix build issues with rollup
    This one took me a while, but it turns out that Rollup requires Typescript to compile to ES Modules
    before it can bundle and tree shake the application. Then, through the rollup/plugin-typescript,
    you can specify the output format for rollup to transpile the ES Module Javascript too. This through me
    for a loop because I needed the final output to be CommonJS and the formatting wasn't correct.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,65 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
+
+const PROJECT_NAME = 'obsidian-to-anki-clozure-conceal';
+const TEST_VAULT_PLUGIN_DIR = `test-vault/.obsidian/plugins/${ PROJECT_NAME }`;
+const BUILD_DIR = 'build';
+
+const BASE_CONFIG = {
+	input: 'src/main.ts',
+	external: ['obsidian', 'path']
+};
+
+const getRollupPlugins = (...plugins) => [
+	typescript({ compilerOptions: { module: "ESNext" }} ), // Rollup requires ES Modules
+	nodeResolve({ browser: true }),
+	commonjs()
+].concat(plugins);
+
+const DEV_PLUGIN_CONFIG = {
+	...BASE_CONFIG,
+	output: {
+		dir: TEST_VAULT_PLUGIN_DIR,
+		sourcemap: 'inline',
+		format: 'cjs',
+		exports: 'auto'
+	},
+	plugins: getRollupPlugins(
+		copy({
+			targets: [
+				{ src: 'manifest.json', dest: `${ TEST_VAULT_PLUGIN_DIR }/` },
+				{ src: 'styles.css', dest: `${ TEST_VAULT_PLUGIN_DIR }/` }
+			]
+		})
+	)
+};
+
+const PROD_PLUGIN_CONFIG = {
+	...BASE_CONFIG,
+	output: {
+		dir: BUILD_DIR,
+		sourcemap: 'inline',
+		sourcemapExcludeSources: true,
+		format: 'cjs',
+		exports: 'auto'
+	},
+	plugins: getRollupPlugins()
+};
 
 // TODO: Create environment specific builds (dev, prod, etc.)
 // Dataview plugin has a good implementaiton of this
-export default {
-	input: 'src/main.ts',
-	output: {
-		file: 'out/main.js',
-		format: 'es',
-		sourcemap: 'inline'
-	},
-	plugins: [typescript()],
-	external: ['obsidian', 'path']
-};
+let configs = [];
+if (process.env.BUILD === 'prod') {
+	// Production build
+	configs.push(PROD_PLUGIN_CONFIG);
+} else if (process.env.BUILD === 'dev') {
+	// Dev build
+	configs.push(DEV_PLUGIN_CONFIG);
+} else {
+	// Default to the dev build.
+	configs.push(DEV_PLUGIN_CONFIG);
+}
+
+export default configs;
