366c0b0e93a8be18fa5b7b7a894112595a601a54
mattcoleanderson <github@mattcoleanderson.com>
2023-11-18 22:53:17 -0600

    Create skeleton for editor extension

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,34 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin } from 'obsidian';
-import { PluginSettings } from './interfaces/pluginSettings';
-import { SettingsTab } from './settingsTab';
-import { Decoration } from '@codemirror/view';
-import { markdownPostProcessor } from './markdownPostProcessor';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin } from "obsidian";
+import { PluginSettings } from "./interfaces/pluginSettings";
+import { SettingsTab } from "./settingsTab";
+import { Decoration, PluginValue, ViewPlugin } from "@codemirror/view";
+import { markdownPostProcessor } from "./markdownPostProcessor";
+import { ConcealViewPlugin } from "./editorExtension";
+import { Extension } from "@codemirror/state";
 
-// Settings 
+// Settings
 // TODO: Add Settings
 const DEFAULT_SETTINGS: PluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 // TODO: Add Resources
 export default class ObsidianToAnkiClozureConcealPlugin extends Plugin {
 	settings: PluginSettings;
-
+	editorExtensions: Extension = ViewPlugin.fromClass(ConcealViewPlugin);
 
 	// Configures resources need by the plugin
 	async onload() {
 		await this.loadSettings();
-		console.log('Loading Obsidian To Anki Clozure Conceal Plugin')
+		console.log("Loading Obsidian To Anki Clozure Conceal Plugin");
 
-		this.registerMarkdownPostProcessor(markdownPostProcessor)
+		this.registerMarkdownPostProcessor(markdownPostProcessor);
+		this.registerEditorExtension(this.editorExtensions);
 	}
 
 	// Releases any resources configured by the plugin
 	onunload() {
-		console.log('Unloading Obsidian To Anki Clozure Conceal Plugin')
-
+		console.log("Unloading Obsidian To Anki Clozure Conceal Plugin");
 	}
 
 	async loadSettings() {
@@ -44,13 +46,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
-
