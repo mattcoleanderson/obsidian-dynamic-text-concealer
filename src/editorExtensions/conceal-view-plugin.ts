27d63fa6c2e342a277b8306fb9a61391a2cb4a2c
matthewanderson <manderson1@firstorion.com>
2023-11-22 13:24:08 -0600

    Disable editor extension in sourcemode

--- src/editorExtensions/conceal-view-plugin.ts
+++ src/editorExtensions/conceal-view-plugin.ts
@@ -1,3 +1,4 @@
+import { StateEffect } from '@codemirror/state';
 import {
 	Decoration,
 	DecorationSet,
@@ -12,11 +13,11 @@ import { editorLivePreviewField } from 'obsidian';
 import { MatchWidget } from './match-widget';
 
 // TODO: Currently the replaced decorater isn't editable. This is undesirable and needs to be fixed.
-// TODO: Stop this plugin from affecting source mode
 // TODO: When in edit mode, the current line should show the conceal text.
 class ConcealViewPlugin implements PluginValue {
 	// TODO: Extract to the settings for the plugin for use throughout the project
-	readonly REGEX_CURLY_MATCH = /{{1,2}(?![\s{])(?:c?\d+(?::{1,2}|\|))?(?<answer>[^}]+)}{1,2}/g;
+	readonly REGEX_CURLY_MATCH =
+		/{{1,2}(?![\s{])(?:c?\d+(?::{1,2}|\|))?(?<answer>[^}]+)}{1,2}/g;
 
 	decorations: DecorationSet; // list of current decorators in view
 	matchDecorator: MatchDecorator; // Creates and updates decorators
@@ -34,18 +35,35 @@ class ConcealViewPlugin implements PluginValue {
 		});
 
 		// Initialize the DecoratorSet if not in source mode
-		this.decorations = this.buildDecorations(view);
+		this.decorations = this.initializeDecorations(view);
 	}
 
 	update(update: ViewUpdate) {
+		const isSourceMode = !update.state.field(editorLivePreviewField);
+		// TODO: Make this a state field
+		const isEditorLayoutChanged = update.transactions.some((t) =>
+			t.effects.some((e) => e.is(workspaceLayoutChangeEffect)),
+		);
+
+		// Reinitialize Decorations if sourc mode or recetly switch back to Live Preview
+		if (isSourceMode || isEditorLayoutChanged) {
+			this.decorations = this.initializeDecorations(update.view);
+			return;
+		}
+
+		// Update DecorationSet with MatchDecorator
 		this.decorations = this.matchDecorator.updateDeco(update, this.decorations);
 	}
 
 	destroy() {}
 
-	private buildDecorations(view: EditorView): DecorationSet {
-		const isLiveMode = view.state.field(editorLivePreviewField);
-		return isLiveMode ? this.matchDecorator.createDeco(view) : Decoration.none;
+	/**
+	 * Initializes DecorationSet. Is disabled if the editor is in source mode.
+	 */
+	private initializeDecorations(view: EditorView): DecorationSet {
+		return view.state.field(editorLivePreviewField) ?
+				this.matchDecorator.createDeco(view)
+			:	Decoration.none;
 	}
 }
 
@@ -55,4 +73,13 @@ const pluginSpec: PluginSpec<ConcealViewPlugin> = {
 
 // concealViewPlugin is the main export for this class
 // and is used to register the editorExtension in main.ts
-export const concealViewPlugin = ViewPlugin.fromClass(ConcealViewPlugin, pluginSpec);
+export const concealViewPlugin = ViewPlugin.fromClass(
+	ConcealViewPlugin,
+	pluginSpec,
+);
+
+/**
+ * A state effect that represents the workspace's layout change.
+ * Mainly intended to detect when the user switches between live preview and source mode.
+ */
+export const workspaceLayoutChangeEffect = StateEffect.define<null>();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-import { App, Editor, editorEditorField, MarkdownView, Modal, Notice, Plugin } from 'obsidian';
+import {
+	App,
+	Editor,
+	editorEditorField,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+} from 'obsidian';
 import { PluginSettings } from './interfaces/pluginSettings';
 import { SettingsTab } from './settingsTab';
-import { Decoration, EditorView, PluginValue, ViewPlugin } from '@codemirror/view';
+import {
+	Decoration,
+	EditorView,
+	PluginValue,
+	ViewPlugin,
+} from '@codemirror/view';
 import { Extension } from '@codemirror/state';
-import { concealViewPlugin, workspaceLayoutChangeEffect } from './editorExtensions/conceal-view-plugin';
+import {
+	concealViewPlugin,
+	workspaceLayoutChangeEffect,
+} from './editorExtensions/conceal-view-plugin';
 import { concealPostProcessor } from './markdownPostProcessors/conceal-post-processor';
 
 // Settings
@@ -23,6 +39,28 @@ export default class ObsidianToAnkiClozureConcealPlugin extends Plugin {
 		this.registerMarkdownPostProcessor(concealPostProcessor);
 
 		this.registerEditorExtension([this.editorExtensions]);
+
+		// TODO: Add obsidian typing for EditorView to Editor
+		// See :
+		//	- https://docs.obsidian.md/Plugins/Editor/Communicating+with+editor+extensions
+		//	- https://github.com/blacksmithgu/obsidian-dataview/pull/2088/files
+		this.registerEvent(
+			this.app.workspace.on('layout-change', () => {
+				this.app.workspace.iterateAllLeaves((leaf) => {
+					if (
+						leaf.view instanceof MarkdownView &&
+						// @ts-expect-error, not typed
+						(leaf.view.editor.cm as EditorView)
+					) {
+						// @ts-expect-error, not typed
+						const cm = leaf.view.editor.cm as EditorView;
+						cm.dispatch({
+							effects: workspaceLayoutChangeEffect.of(null),
+						});
+					}
+				});
+			}),
+		);
 	}
 
 	// Releases any resources configured by the plugin
