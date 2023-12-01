b8092524ee2a447142459ee3474bffe4123f0180
matthewanderson <manderson1@firstorion.com>
2023-12-01 16:48:32 -0600

    create settings tab

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { App, Editor, editorEditorField, MarkdownView, Modal, Notice, Plugin } from 'obsidian';
-import { PluginSettings } from './interfaces/pluginSettings';
+import { PluginSettings } from './interfaces/plugin-settings';
 import { SettingsTab } from './settingsTab';
 import { Decoration, EditorView, PluginValue, ViewPlugin } from '@codemirror/view';
 import { Extension } from '@codemirror/state';
@@ -12,16 +12,23 @@ const DEFAULT_SETTINGS: PluginSettings = {
 	mySetting: 'default',
 };
 
-export default class ObsidianToAnkiClozureConcealPlugin extends Plugin {
+export default class ConcealPlugin extends Plugin {
 	settings: PluginSettings;
 	editorExtensions: Extension = concealViewPlugin; // TODO: Don't hardcode this. Instead, this should be a List of Extensions updated by a function
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 
 	async onload() {
 		await this.loadSettings();
 		console.log('Loading Obsidian To Anki Clozure Conceal Plugin');
 
 		this.registerMarkdownPostProcessor(concealPostProcessor);
-
 		this.registerEditorExtension([this.editorExtensions]);
 
 		// TODO: Add obsidian typing for EditorView to Editor
@@ -45,19 +52,13 @@ export default class ObsidianToAnkiClozureConcealPlugin extends Plugin {
 				});
 			}),
 		);
+
+		this.addSettingTab(new SettingsTab(this.app, this));
 	}
 
 	// Releases any resources configured by the plugin
 	onunload() {
 		console.log('Unloading Obsidian To Anki Clozure Conceal Plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 }
 
--- src/settingsTab.ts
+++ src/settingsTab.ts
@@ -1,28 +1,30 @@
-import { App, PluginSettingTab, Setting } from "obsidian";
-import ObsidianToAnkiClozureConcealPlugin from "./main";
+import { App, PluginSettingTab, Setting } from 'obsidian';
+import ConcealPlugin from './main';
 
 export class SettingsTab extends PluginSettingTab {
-	plugin: ObsidianToAnkiClozureConcealPlugin;
+	plugin: ConcealPlugin;
 
-	constructor(app: App, plugin: ObsidianToAnkiClozureConcealPlugin) {
+	constructor(app: App, plugin: ConcealPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	async display() {
 		// This is the outtermost HTML element on the setting tab
-		const {containerEl} = this;
+		const { containerEl } = this;
 		containerEl.empty();
 
 		new Setting(containerEl)
 			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder('Enter your secret')
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
