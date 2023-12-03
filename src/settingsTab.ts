9fa0f16e68b2fb7a6d77dba9b1cc102aa9fefcad
mattcoleanderson <github@mattcoleanderson.com>
2023-12-03 12:42:10 -0600

    Reconfigure custom regex settings

--- src/settingsTab.ts
+++ src/settingsTab.ts
@@ -9,64 +9,78 @@ export class SettingsTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
-	// setupRegexp(headerRow: HTMLTableRowElement, bodyRow: HTMLTableRowElement) {
-	// 	// Table Header
-	// 	headerRow.createEl('th', { text: 'REGEXP' });
-	// 	// Table Body
-	// 	let cell = bodyRow.createEl('td');
-	// 	let setting = new Setting(cell).addText((text) =>
-	// 		text.setValue(this.plugin.settings.regexp).onChange(async (value) => {
-	// 			this.plugin.settings.regexp = value;
-	// 			await this.plugin.saveSettings();
-	// 		}),
-	// 	);
-	// }
-
-	// setupEnable(headerRow: HTMLTableRowElement, bodyRow: HTMLTableRowElement) {
-	// 	// Table Header
-	// 	headerRow.createEl('th', { text: 'Enable' });
-	// 	// Table Body
-	// 	let cell = bodyRow.createEl('td');
-	// 	let setting = new Setting(cell).addToggle((toggle) =>
-	// 		toggle.setValue(this.plugin.settings.enable).onChange(async (value) => {
-	// 			this.plugin.settings.enable = value;
-	// 			await this.plugin.saveSettings();
-	// 		}),
-	// 	);
-	// }
-
-	// setupMatchTable() {
-	// 	this.containerEl.createEl('h3', { text: 'Conceal Patterns' });
-	// 	this.containerEl.createEl('p', { text: 'Each row is a REGEX pattern for concealing matched text.' });
-
-	// 	// Table
-	// 	let matchTable = this.containerEl.createEl('table', { cls: 'conceal-settings-match-table' });
-	// 	// Table Header
-	// 	let headerRow = matchTable.createTHead().insertRow();
-	// 	// Table Body
-	// 	let tableBody = matchTable.createTBody();
-	// 	let bodyRow = tableBody.insertRow();
-
-	// 	this.setupRegexp(headerRow, bodyRow);
-	// 	this.setupEnable(headerRow, bodyRow);
-	// }
+	private arrayMove<T>(arr: T[], fromIndex: number, toIndex: number): void {
+		if (toIndex < 0 || toIndex === arr.length) {
+			return;
+		}
+		const element = arr[fromIndex];
+		arr[fromIndex] = arr[toIndex];
+		arr[toIndex] = element;
+	}
 
 	addRegexSettings() {
 		this.containerEl.createEl('h2', { text: 'Regex Expressions' });
 		this.containerEl.createEl('p', { text: 'Custom regex allows you to specify patterns to conceal' });
 
 		this.plugin.settings.regexp.forEach((regex, index) => {
-			const setting = new Setting(this.containerEl).addText((text) => {
-				text.setValue(regex).onChange((newRegex) => {
-					if (newRegex && this.plugin.settings.regexp.contains(newRegex)) {
-						// TODO: Log Error
-						return;
-					}
-					this.plugin.settings.regexp[index] = newRegex;
-					this.plugin.saveSettings();
-					this.plugin.updateEditorExtension();
+			const setting = new Setting(this.containerEl)
+				.addText((text) => {
+					text.setValue(regex).onChange((newRegex) => {
+						if (newRegex && this.plugin.settings.regexp.contains(newRegex)) {
+							// TODO: Log Error
+							return;
+						}
+						this.plugin.settings.regexp[index] = newRegex;
+						this.plugin.saveSettings();
+						this.plugin.updateEditorExtension();
+					});
+				})
+				.addExtraButton((button) => {
+					button
+						.setIcon('up-chevron-glyph')
+						.setTooltip('Move up')
+						.onClick(() => {
+							console.log('Oh No!');
+							this.arrayMove(this.plugin.settings.regexp, index, index - 1);
+							this.plugin.saveSettings();
+							this.display();
+							this.plugin.updateEditorExtension();
+						});
+				})
+				.addExtraButton((button) => {
+					button
+						.setIcon('down-chevron-glyph')
+						.setTooltip('Move down')
+						.onClick(() => {
+							this.arrayMove(this.plugin.settings.regexp, index, index + 1);
+							this.plugin.saveSettings();
+							this.display();
+							this.plugin.updateEditorExtension();
+						});
+				})
+				.addExtraButton((button) => {
+					button
+						.setIcon('cross')
+						.setTooltip('Delete')
+						.onClick(() => {
+							this.plugin.settings.regexp.splice(index, 1);
+							this.plugin.saveSettings();
+							this.plugin.updateEditorExtension();
+							this.display();
+						});
 				});
-			});
+			setting.infoEl.remove();
+		});
+
+		new Setting(this.containerEl).addButton((button) => {
+			button
+				.setButtonText('Add new regular expression')
+				.setCta()
+				.onClick(() => {
+					this.plugin.settings.regexp.push('');
+					this.plugin.saveSettings();
+					this.display();
+				});
 		});
 	}
 
